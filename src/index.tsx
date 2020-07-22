eddeca0627573c070c41cd165f652ce9a8d4a7f8
Vladislav Ilin <ilin96inbox@gmail.com>
2020-07-22 21:33:51 +0900

    add fb

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { defineCustomElements } from '@ionic/pwa-elements/loader';
+import React from "react";
+import ReactDOM from "react-dom";
+import { defineCustomElements } from "@ionic/pwa-elements/loader";
+import * as serviceWorker from "./serviceWorker";
 
-import App from './App';
-import MemoriesContextProvider from './data/MemoriesContextProvider';
+import App from "./App";
+import MemoriesContextProvider from "./data/MemoriesContextProvider";
 
 ReactDOM.render(
   <MemoriesContextProvider>
     <App />
   </MemoriesContextProvider>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 defineCustomElements(window);
+
+serviceWorker.register();
